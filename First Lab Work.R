6d0feb48d6f9df7a29b038d375b5d9b7ab098619
Abtahi Islam <143752829+Abtahi360@users.noreply.github.com>
2026-02-28 00:16:59 +0600

    First Commit

---
+++ First Lab Work.R
@@ -0,0 +1,69 @@
+a<- c(1,2,3,4,5)
+f <- 1:100
+b<- c(5)
+d<- 4
+e<- c(6,7,8,9,10,NA)
+a+e
+a-e
+sum(a)
+sum(e, na.rm = TRUE)
+
+mean(a)
+mode(a)
+var(a)  # variance
+median(a)
+sd(a)   # standard deviation
+
+ch_vec <- c("apple", "banana")
+
+num_vec <- c(10,20,30,40,50)
+num_vec[3]
+num_vec[3] <- 35
+
+num_vec[c(3,5)] <- c(300, 500)
+
+num_vec <- num_vec[num_vec>30]
+
+
+# Date: 26/02 -- Thursday 
+
+sorted_vec <- sort(e, decreasing = TRUE)
+print(sorted_vec)
+
+# New
+seq_vec <- seq(1, 10, by = 2)
+print(seq_vec)
+
+seq_vec <- seq(5, 30, by = 5)
+print(seq_vec)
+
+seq_vec <- seq(5, 30, 5)
+print(seq_vec)
+
+# New
+rep_vec <- rep(c(1, 2, 3), times = 3)
+print(rep_vec)
+
+print(rep(c(1, 2, 3, 4), times = c(1, 2, 3, 4)))
+
+print(rep(c(1:4), times = c(1:4)))
+print(rep(1:4, 1:4))
+
+# New
+mat <- matrix(1:9, nrow = 3, ncol = 3)
+print(mat)
+
+mat <- matrix(1:9, nrow = 3, ncol = 3, byrow = TRUE)
+print(mat)
+
+mat <- matrix(c(3, 1, 3, 14, 2, 0, 50, 1, 5), nrow = 3, ncol = 3)
+print(mat)
+rownames(mat) <- c("Row1", "Row2", "Row3")
+colnames(mat) <- c("Col1", "Col2", "Col3")
+print(mat)
+
+print(mat[2:3, 2:3])
+
+mat[c(1, 3), c(1, 3)]
+
+
